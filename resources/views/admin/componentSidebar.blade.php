6c4fe80bfa6fb61d2e031044f500e657bd919d58
leikou <leikou@eaglemobi.cc>
2019-03-04 12:21:29 +0800

    Add banner url in navigation bar

--- resources/views/admin/componentSidebar.blade.php
+++ resources/views/admin/componentSidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="col-sm-3 col-md-2 sidebar">
   <ul class="nav nav-sidebar">
     <li @if ($navActive == 'list') class="active" @endif>
-        <a href="/admin/clients">列表</a>
+        <a href="/admin/banners">Banner管理</a>
     </li>
     <li @if ($navActive == 'new') class="active" @endif>
         <a href="/admin/clients/create-form">新增 <span class="sr-only">(current)</span></a>
